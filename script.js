b378c6a98c17bf59901393fba69d99b480c555e0
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-12 22:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+function findFirstNonRepeatedCharacter(inputString) {
+      const charCount = {};
+
+      // Count the occurrences of each character in the string
+      for (let i = 0; i < inputString.length; i++) {
+        const char = inputString[i];
+        charCount[char] = (charCount[char] || 0) + 1;
+      }
+
+      // Find the first non-repeated character
+      for (let i = 0; i < inputString.length; i++) {
+        const char = inputString[i];
+        if (charCount[char] === 1) {
+          return char;
+        }
+      }
+
+      // If there is no non-repeated character, return null
+      return null;
+    }
+
+    // Get input string using prompt
+    const inputString = prompt("Enter a string:");
+    const firstNonRepeatedChar = findFirstNonRepeatedCharacter(inputString);
+
+    if (firstNonRepeatedChar !== null) {
+      alert(firstNonRepeatedChar);
+    } 
